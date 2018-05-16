2bdec219d52d1977a8795125fe242dc7ae1bacc9
Jan <jan.ficko@gmail.com>
2018-05-16 22:10:11 +0200

    Changed post method in sendImage

--- src/providers/post-service/post-service.ts
+++ src/providers/post-service/post-service.ts
@@ -30,7 +30,7 @@ export class PostServiceProvider {
   sendImage(eventId: string, image: FormData) {
     // Ko pošiljaš sliko ji moraš določit key "image"
     return new Promise((resolve, reject) => {
-      this.http.post(this.apiUrl, JSON.stringify( {"id_dogodek": eventId, "image": image}))
+      this.http.post(this.apiUrl, "{id_dogodek:"+ eventId  +", image:" +image+"}")
         .subscribe(res => {
           resolve(res);
         }, (err) => {
